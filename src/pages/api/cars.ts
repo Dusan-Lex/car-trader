c7bd895292609b60efd399d9224c930a4e46d007
Dusan-Lex <dusanlexmilosavljevic@gmail.com>
2022-03-01 23:23:30 +0100

    finished car trader

--- src/pages/api/cars.ts
+++ src/pages/api/cars.ts
@@ -1,6 +1,6 @@
 import { ObjectId } from "mongodb";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { connectToDatabase } from "../../utils/openDB.js";
+import { getPaginatedCars } from "../../utils/getPaginatedCars";
 
 export interface CarModel {
   _id: ObjectId;
@@ -14,22 +14,21 @@ export interface CarModel {
   photo_url: string;
 }
 
-const handler = async (
-  req: NextApiRequest,
-  res: NextApiResponse<CarModel[] | { message: string }>
-) => {
-  try {
-    const { db } = await connectToDatabase();
-    const result = (await db
-      .collection("cars")
-      .find({})
-      .toArray()) as CarModel[];
-    res.status(200).send(result);
-  } catch (err: unknown) {
-    if (err instanceof Error) {
-      res.status(500).send({ message: err.message });
-    }
-  }
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const cars = await getPaginatedCars(req.query);
+  res.json(cars);
+  // try {
+  //   const { db } = await connectToDatabase();
+  //   const result = (await db
+  //     .collection("cars")
+  //     .find({})
+  //     .toArray()) as CarModel[];
+  //   res.status(200).send(result);
+  // } catch (err: unknown) {
+  //   if (err instanceof Error) {
+  //     res.status(500).send({ message: err.message });
+  //   }
+  // }
 };
 
 export default handler;
